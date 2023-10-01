7d06843d545f98ebee07da610a65d0302fb99c63
tony-brian <btoni3823@gmail.com>
2023-10-01 14:10:09 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -19,22 +19,53 @@ class Signup(Resource):
     
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
+        username = json.get('username')
+        password = json.get('password')
+        
+        if not username or not password:
+            return {'message': 'Both username and password are required'}, 400
+
+        
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return {'message': 'Username already exists'}, 400
+
+
+        user = User(username=username)
+        user.password_hash = password  
         db.session.add(user)
         db.session.commit()
+
+        
+        session['user_id'] = user.id
+
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+        return {}, 204  
 
 class Login(Resource):
-    pass
+    def post(self):
+        json_data = request.get_json()
+        username = json_data.get('username')
+        password = json_data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200  
+        return {'message': 'Invalid credentials'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop('user_id', None)
+        return {'message': '204: No Content'}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
